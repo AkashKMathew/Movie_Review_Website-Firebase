7ae612bf106ac566e9a5bb0282d2f069765c3e50
AkashKMathew <akashkmathew156@gmail.com>
2023-11-23 23:58:54 +0530

    firestore implimentation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import './App.css';
 import Login from './components/login.js';
+import { useState,useEffect } from 'react';
+import {db} from "./config/firebase.js"
+import { getDocs,collection } from 'firebase/firestore';
 
 function App() {
-  // console.log(login);
+  const [movieList, setMovieList] = useState([]);
+  const moviesCollectionRef = collection(db,"movies");
+  useEffect(() => {
+    const getMovieList=async()=>{
+       
+       try{
+        const data = await getDocs(moviesCollectionRef);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setMovieList(filteredData);
+       }catch(err){
+        console.error(err);
+       }
+    };
+
+    getMovieList();
+  }, []);
+  
+  
   return (
     <div className="App">
       <Login/>
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore} from 'firebase/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,4 +24,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export const api_key=process.env.REACT_APP_API_KEY;
+export const db = getFirestore(app);
