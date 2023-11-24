480b547821171060532df7a2b5eb57e41cd6b757
AkashKMathew <akashkmathew156@gmail.com>
2023-11-24 23:53:51 +0530

    addDoc

--- src/App.js
+++ src/App.js
@@ -1,34 +1,81 @@
-import './App.css';
-import Login from './components/login.js';
-import { useState,useEffect } from 'react';
-import {db} from "./config/firebase.js"
-import { getDocs,collection } from 'firebase/firestore';
+import "./App.css";
+import Login from "./components/login.js";
+import { useState, useEffect } from "react";
+import { db } from "./config/firebase.js";
+import { getDocs, collection, addDoc, deleteDoc } from "firebase/firestore";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
-  const moviesCollectionRef = collection(db,"movies");
-  useEffect(() => {
-    const getMovieList=async()=>{
-       
-       try{
-        const data = await getDocs(moviesCollectionRef);
-        const filteredData = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        setMovieList(filteredData);
-       }catch(err){
-        console.error(err);
-       }
-    };
 
+  const [newMovieName, setNewMovieName] = useState("");
+  const [newReleaseDate, setNewReleaseDate] = useState(0);
+  const [newMovieOscar, setNewMovieOscar] = useState(false);
+
+  const moviesCollectionRef = collection(db, "movies");
+
+  const getMovieList = async () => {
+    try {
+      const data = await getDocs(moviesCollectionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setMovieList(filteredData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
     getMovieList();
   }, []);
-  
-  
+  const submitMovieData = async () => {
+    console.log(newMovieName, newMovieOscar, newReleaseDate);
+    try {
+      await addDoc(moviesCollectionRef, {
+        name: newMovieName,
+        releaseDate: newReleaseDate,
+        receivedOscar: newMovieOscar,
+      });
+      getMovieList();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="App">
-      <Login/>
+      <Login />
+
+      <div>
+        <input
+          placeholder="Movie Name"
+          onChange={(e) => setNewMovieName(e.target.value)}
+        />
+        <input
+          placeholder="Release Date"
+          type="number"
+          onChange={(e) => setNewReleaseDate(Number(e.target.value))}
+        />
+        <input
+          type="checkbox"
+          checked={newMovieOscar}
+          onChange={(e) => setNewMovieOscar(e.target.checked)}
+        />
+        <label>Received Oscar</label>
+        <button onClick={submitMovieData}>Submit</button>
+      </div>
+
+      <div>
+        {movieList.map((movie) => (
+          <div>
+            <h1 style={{ color: movie.receivedOscar ? "green" : "red" }}>
+              {movie.name}
+            </h1>
+            <p>Date: {movie.releaseDate}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
